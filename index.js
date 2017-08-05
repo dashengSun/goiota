ba7784d4f43695f66dd94ec09d29eeae184c816c
kaichao <kaichaosuna@gmail.com>
2017-08-05 09:27:13 +0800

    format.

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-// Create IOTA instance with host and port as provider
-//var iota = new IOTA({
-//    'host': 'http://localhost',
-//    'port': 14265
-//});
-//
-// Create IOTA instance directly with provider
-
 var IOTA = require("iota.lib.js")
 var iota = new IOTA({
     'provider': 'http://yourhost:yourport'
